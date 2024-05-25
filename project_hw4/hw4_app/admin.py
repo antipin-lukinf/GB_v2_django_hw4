64a54a8b006742b51017a505e8f508d1e99ecf33
Drei4 <antipin-lukinf@mail.ru>
2024-05-25 20:57:11 +0300

    DZ_5 complit

--- project_hw4/hw4_app/admin.py
+++ project_hw4/hw4_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Client, Product, Order
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone_number', 'address', 'registration_date')
+    search_fields = ('name', 'email')
+    list_filter = ('order__products', 'order__order_date')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'price', 'quantity', 'added_date')
+    search_fields = ('name', 'description')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'total_amount', 'order_date')
+    search_fields = ('client__name', 'id')
+    filter_horizontal = ('products',)
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Order, OrderAdmin)
